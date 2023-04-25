27fd0298ae16278a66e8911d688199341dd958d9
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-04-25 09:19:17 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make alphabet in lowercase
+ * followed by a new line
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
