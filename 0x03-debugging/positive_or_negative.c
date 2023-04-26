e34e98e5ad414846f1c78626525a5c5076cc0b39
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-04-26 03:47:54 -0700

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,23 +2,22 @@
 
 /**
  * positive_or_negative - determines if number is positive, negative or zero
- * Return: 0
+ * @i: the number to be checked
+ * Return: void
  */
 
 void positive_or_negative(int i)
 {
-
-        if (i > 0)
-{
-        printf("%d is positive\n", i);
-}
-        else if (i < 0)
-{
-        printf("%d is negative\n", i);
-}
-        else
-{
-        printf("%d is zero\n", i);
-}
-return;
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
 }
