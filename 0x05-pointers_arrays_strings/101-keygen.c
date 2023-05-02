add69b46829edc2a66eb1734f2e3bc74c3a57349
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-05-02 14:28:35 -0700

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,24 +10,14 @@
 int main(void)
 {
 	char password[7];
-	int i, sum, n;
-
-	sum = 0;
+	int i;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 7; i++) 
 	{
-		password[i] = rand() % 78;
-		sum += (password[i] + '0');
-		putchar(password[i] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		password[i] = rand() % 94 + 33;
 	}
-	return (0);
+	printf("%s\n", password);
+	return 0;
 }
