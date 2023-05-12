435f98ca97c3837e4eeefadfe51cd330609bcbff
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-05-12 12:47:21 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,12 @@
 /**
  * main - prints name of the program
  *
- * return: always 0
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
