891a7d6190daf29446ddd0bfe4c1ced5f690e2dd
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-05-15 23:53:39 -0700

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - frees a 2 dimensional grid previously created by
  * your alloc_grid function
- * @**grid: grid pointer
+ * @grid: grid pointer
  * @height: of the array
  * Return: nothing
  */
