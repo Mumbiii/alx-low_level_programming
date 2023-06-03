39a082b13021f9c99e4d28d3aef0dd5cc19614c1
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-06-03 03:00:15 -0700

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,21 @@
 section .data
-	format db "Hello, Holberton", 0
-	fmt db "%s", 0
+    format db "Hello, Holberton", 0
+    newline db 10, 0
 
 section .text
-	global main
-	extern printf
+    global main
+    extern printf
 
 main:
-	; Call printf to print the string
-	mov edi, fmt
-	mov esi, format
-	xor eax, eax
-	call printf
+    sub rsp, 8    ; Adjust stack to align it to 16 bytes (8-byte return address + 8-byte alignment)
+    mov edi, format
+    xor eax, eax
+    call printf
 
-	; Exit the program
-	xor edi, edi
-	mov eax, 60
-	syscall
+    mov edi, newline
+    xor eax, eax
+    call printf
+
+    add rsp, 8    ; Restore the stack pointer
+    xor eax, eax  ; Return 0 from the main function
+    ret
