7baeb17d3987eedb343bfb97b50e9685e239c43f
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-06-03 02:00:41 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,28 +10,18 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 	size_t count = 0;
-	list_t *temp = *head;
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	while (str[count])
+		count++;
+
 	new_node->len = count;
 	new_node->str = strdup(str);
-
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	new_node->next = *head;
 	*head = new_node;
-
-	while (temp != NULL)
-	{
-		count++;
-		temp = temp->next;
-	}
 
 	return (new_node);
 }
