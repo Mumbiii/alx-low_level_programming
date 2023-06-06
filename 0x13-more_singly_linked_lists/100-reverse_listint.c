590ab4f4dd716e2876d140b0eb22b10612c965a6
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-06-06 00:12:00 -0700

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,19 +9,17 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (*head);
-
-	while (current != NULL)
+	while (*head)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
+
 	*head = prev;
+
 	return (*head);
 }
