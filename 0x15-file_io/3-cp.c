1a848687dba312813c507bbdca964dde4150b442
Mary Mumbi <nyokabimumbi24@gmail.com>
2023-06-12 08:22:07 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
+#define BUF_SIZE 1024
 
 /**
  * main - Copies the content of a file to another file.
@@ -15,56 +15,87 @@
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to, read_count, write_count;
-	char buffer[BUFFER_SIZE];
+	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = malloc(sizeof(char) *BUF_SIZE);
+	if (!buffer)
+		return (0);
 	fd_from = open(argv[1], O_RDONLY);
+	error_98(fd_from,  buffer, argv[1]);
 
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	error_99(fd_to,  buffer, argv[2]);
 
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
-	while ((read_count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
+	do {
+		read_count = read(fd_from, buffer, BUF_SIZE);
+		if (read_count == 0)
+			break;
+		error_98(read_count,  buffer, argv[1]);
 		write_count = write(fd_to, buffer, read_count);
-
-		if (write_count != read_count)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-	if (read_count == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		error_99(write_count,  buffer, argv[2]);
+	} while (write_count >= BUF_SIZE);
+	read_count = close(fd_to);
+	error_100(read_count, buffer);
+	read_count = close(fd_from);
+	error_100(read_count, buffer);
+	free(buffer);
 	return (0);
+}
+
+/**
+ * error_98 - checks 98
+ * @fd_to: value to check
+ * @buffer: the  buffer
+ * @argv: argument
+ * Return: nothing
+ */
+
+void error_98(int fd_to, char *buffer, char *argv)
+{
+	 if (fd_to < 0)
+	 {
+		 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		 free(buffer);
+		 exit(98);
+	 }
+}
+/**
+ * error_99 - checks 99
+ * @fd_to: value to check
+ * @buffer: the  buffer
+ * @argv: argument
+ * Return: nothing
+ */
+
+void error_99(int fd_to, char *buffer, char *argv)
+{
+         if (fd_to < 0)
+         {
+                 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv);
+                 free(buffer);
+                 exit(99);
+         }
+}
+}
+/**
+ * error_100 - checks 100
+ * @fd_to: value to check
+ * @buffer: the  buffer
+ *
+ * Return: nothing
+ */
+
+void error_100(int fd_to, char *buffer)
+{
+         if (fd_to < 0)
+         {
+                 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fd_to);
+                 free(buffer);
+                 exit(100);
+         }
 }
