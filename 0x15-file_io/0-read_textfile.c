11a65a8ce04a9a6a23df15ab7c23f53468d8a60a
Mary Mmumbi <nyokabimumbi24@gmail.com>
2023-07-10 07:58:29 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,39 +11,19 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
 	char *buffer;
-	ssize_t read_count, write_count;
+	ssize_t fd;
+	ssize_t writee;
+	ssize_t readd;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	readd = read(fd, buffer, letters);
+	writee = write(STDOUT_FILENO, buffer, readd);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
-	read_count = fread(buffer, sizeof(char), letters, file);
-	if (read_count <= 0)
-	{
-		free(buffer);
-		fclose(file);
-		return (0);
-	}
-	write_count = fwrite(buffer, sizeof(char), read_count, stdout);
-	if (write_count < read_count)
-	{
-		free(buffer);
-		fclose(file);
-		return (0);
-	}
-	fclose(file);
 	free(buffer);
-
-	return (read_count);
+	close(fd);
+	return (writee);
 }
